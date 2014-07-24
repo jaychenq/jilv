528bdb7d7d20b5d7abb8b800fed7dd2ac8c6a5a4
Jason <jason0037@21cn.com>
2014-07-24 22:48:11 +0800

    bugfix

--- app/models/travel/journey.rb
+++ app/models/travel/journey.rb
@@ -9,7 +9,7 @@ class Travel::Journey < ActiveRecord::Base
   validates :name, :fee_description, :refund_policy, presence: true
   
   cattr_accessor :admin_fields, :business_fields
-  self.business_fields = %w[ product_id name sequence market_price adult_price child_price people_quota started_on ended_on start_time duration_time early_booking pick_up with_food introduction fee_description child_level refund_policy other_description route tips published ]
+  self.business_fields = %w[ product_id name sequence lowest_price market_price adult_price child_price people_quota started_on ended_on start_time duration_time early_booking pick_up with_food introduction fee_description child_level refund_policy other_description route tips published ]
   self.admin_fields = self.business_fields + %w[]
   
   before_validation do
