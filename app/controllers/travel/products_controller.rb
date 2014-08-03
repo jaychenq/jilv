161033cdc5d7fced80847a15aae24575b08f1cf2
Jason <jason0037@21cn.com>
2014-08-04 01:13:05 +0800

    bugfix

--- app/controllers/travel/products_controller.rb
+++ app/controllers/travel/products_controller.rb
@@ -1,7 +1,10 @@
 class Travel::ProductsController < Travel::ApplicationController
   def index
     @products = model.where(published: true).where(params[:where].to_h.slice(*%w[merchant_id continent_id country_id city_id category1_id category2_id])).order(updated_at: :desc).paginate(page: params[:page], per_page: params[:per_page] || 10)
+    @products_all = model.where(published: true).order(updated_at: :desc).paginate(page: params[:page], per_page: params[:per_page] || 10)
+    @products_for_city = model.where(published: true).where(params[:where].to_h.slice(*%w[country_id])).order(updated_at: :desc).paginate(page: params[:page], per_page: params[:per_page] || 10)
     @products = @products.joins("INNER JOIN travel_speakings ON travel_speakings.product_id = travel_products.id AND travel_speakings.active = TRUE AND travel_speakings.language_id = #{params[:where][:language_id].to_i}") if params[:where] && params[:where][:language_id]
+  	render :layout => "product"
   end
   
   def show
