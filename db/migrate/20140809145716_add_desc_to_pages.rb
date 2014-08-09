7ddeef639de4324cecec93a6837562e68d407ba1
Jason <jason0037@21cn.com>
2014-08-10 01:31:29 +0800

    bugfix

--- db/migrate/20140809145716_add_desc_to_pages.rb
+++ db/migrate/20140809145716_add_desc_to_pages.rb
@@ -1,8 +1,8 @@
 class AddDescToPages < ActiveRecord::Migration
   def up
-  	add_column :about_pages,:desc,:string
+  	add_column :about_pages,:desc,:text
   end
   def down
-  	remove_column :about_pages,:desc,:string
+  	remove_column :about_pages,:desc
   end
 end
